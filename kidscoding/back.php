dd0060f7114a4f6c75971b31f21b8dba817f5fc9
Angie <liya0706s@gmail.com>
2024-01-17 15:33:54 +0800

    kidscoding back的單引號update

--- kidscoding/back.php
+++ kidscoding/back.php
@@ -35,7 +35,7 @@ if (!isset($_SESSION['login'])) {
 <body>
   <div id="cover" style="display:none; ">
     <div id="coverr">
-      <a style="position:absolute; right:3px; top:4px; cursor:pointer; z-index:9999;" onclick="cl(&#39;#cover&#39;)">X</a>
+      <a style="position:absolute; right:3px; top:4px; cursor:pointer; z-index:9999;" onclick="cl('#cover')">X</a>
       <div id="cvr" style="position:absolute; width:99%; height:100%; margin:auto; z-index:9898;"></div>
     </div>
   </div>
